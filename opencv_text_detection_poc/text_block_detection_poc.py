3b02f35ec27dd6c0afa865adf9349658a5004a6d
Rohith Bollineni <rohith.bolline@gmail.com>
2022-09-22 20:44:40 -0400

    Runs text detection code without error, but output is highly inaccurate

--- opencv_text_detection_poc/text_block_detection_poc.py
+++ opencv_text_detection_poc/text_block_detection_poc.py
@@ -3,26 +3,84 @@ import math
 from pdf2image import convert_from_path
 import numpy as np
 
+
 # Following tutorial here: https://stackoverflow.com/a/57044083
+
+# Decode method pulled from 
+# https://github.com/opencv/opencv/blob/master/samples/dnn/text_detection.py 
+def decodeBoundingBoxes(scores, geometry, scoreThresh):
+    detections = []
+    confidences = []
+
+    ############ CHECK DIMENSIONS AND SHAPES OF geometry AND scores ############
+    assert len(scores.shape) == 4, "Incorrect dimensions of scores"
+    assert len(geometry.shape) == 4, "Incorrect dimensions of geometry"
+    assert scores.shape[0] == 1, "Invalid dimensions of scores"
+    assert geometry.shape[0] == 1, "Invalid dimensions of geometry"
+    assert scores.shape[1] == 1, "Invalid dimensions of scores"
+    assert geometry.shape[1] == 5, "Invalid dimensions of geometry"
+    assert scores.shape[2] == geometry.shape[2], "Invalid dimensions of scores and geometry"
+    assert scores.shape[3] == geometry.shape[3], "Invalid dimensions of scores and geometry"
+    height = scores.shape[2]
+    width = scores.shape[3]
+    for y in range(0, height):
+
+        # Extract data from scores
+        scoresData = scores[0][0][y]
+        x0_data = geometry[0][0][y]
+        x1_data = geometry[0][1][y]
+        x2_data = geometry[0][2][y]
+        x3_data = geometry[0][3][y]
+        anglesData = geometry[0][4][y]
+        for x in range(0, width):
+            score = scoresData[x]
+
+            # If score is lower than threshold score, move to next x
+            if (score < scoreThresh):
+                continue
+
+            # Calculate offset
+            offsetX = x * 4.0
+            offsetY = y * 4.0
+            angle = anglesData[x]
+
+            # Calculate cos and sin of angle
+            cosA = math.cos(angle)
+            sinA = math.sin(angle)
+            h = x0_data[x] + x2_data[x]
+            w = x1_data[x] + x3_data[x]
+
+            # Calculate offset
+            offset = ([offsetX + cosA * x1_data[x] + sinA * x2_data[x], offsetY - sinA * x1_data[x] + cosA * x2_data[x]])
+
+            # Find points for rectangle
+            p1 = (-sinA * h + offset[0], -cosA * h + offset[1])
+            p3 = (-cosA * w + offset[0], sinA * w + offset[1])
+            center = (0.5 * (p1[0] + p3[0]), 0.5 * (p1[1] + p3[1]))
+            detections.append((center, (w, h), -1 * angle * 180.0 / math.pi))
+            confidences.append(float(score))
+
+    # Return detections and confidences
+    return [detections, confidences]
+
 
 # There are other model options we can try out. Download east from here: 
 # https://www.dropbox.com/s/r2ingd0l3zt8hxs/frozen_east_text_detection.tar.gz?dl=1
 net = cv2.dnn.readNet("../../frozen_east_text_detection.pb")
 image_name = "SAMPLE_IMAGES_CybergyHoldings_AffiliateAgreement/pg2.jpg"
-frame = cv2.imread(image_name, 0)
+frame = cv2.imread(image_name)
+original_image = frame.copy()
 
 # Open a sample of the image. Press any key to close
-cv2.imshow('image', frame)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.imshow('image', frame)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
 inpWidth = inpHeight = 320  # A default dimension
 # Preparing a blob to pass the image through the neural network
 # Subtracting mean values used while training the model.
 image_blob = cv2.dnn.blobFromImage(frame, 1.0, (inpWidth, inpHeight), (123.68, 116.78, 103.94), True, False)
 
-# Change number of input channels to 1 for grayscale input
-# (currently throws error expecting colored image)
 output_layer = []
 output_layer.append("feature_fusion/Conv_7/Sigmoid")
 output_layer.append("feature_fusion/concat_3")
@@ -34,5 +92,31 @@ geometry = output[1]
 
 confThreshold = 0.5
 nmsThreshold = 0.3
-[boxes, confidences] = cv2.dnn.NMSBoxes(scores, geometry, confThreshold)
+[boxes, confidences] = decodeBoundingBoxes(scores, geometry, confThreshold)
 indices = cv2.dnn.NMSBoxesRotated(boxes, confidences, confThreshold, nmsThreshold)
+
+height_ = frame.shape[0]
+width_ = frame.shape[1]
+rW = width_ / float(inpWidth)
+rH = height_ / float(inpHeight)
+
+# Draw bounding boxes
+for i in indices:
+    # get 4 corners of the rotated rect
+    vertices = cv2.boxPoints(boxes[i])
+    # scale the bounding box coordinates based on the respective ratios
+    for j in range(4):
+        vertices[j][0] *= rW
+        vertices[j][1] *= rH
+    for j in range(4):
+        p1 = (int(vertices[j][0]), int(vertices[j][1]))
+        p2 = (int(vertices[(j + 1) % 4][0]), int(vertices[(j + 1) % 4][1]))
+        cv2.line(frame, p1, p2, (0, 255, 0), 3)
+
+# To save the image:
+cv2.imwrite("boxed_image.jpg", frame)
+
+cv2.imshow('image', frame)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
